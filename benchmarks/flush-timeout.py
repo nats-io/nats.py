4e573e10799d96f29d987d28be183091dbfbb4f5
Waldemar Quevedo <wally@apcera.com>
2016-01-07 01:13:58 -0800

    Add benchmark scripts

---
+++ benchmarks/flush-timeout.py
@@ -0,0 +1,85 @@
+import asyncio
+import json
+import sys
+
+import http.client
+import time
+from nats.aio.client import Client as NATS
+from nats.aio.errors import ErrConnectionClosed, ErrTimeout
+
+def run(loop):
+    nc = NATS()
+
+    errors = 0
+    @asyncio.coroutine
+    def error_cb(e):
+        nonlocal errors
+        errors += 1
+
+    options = {
+        "servers": ["nats://127.0.0.1:4225"],
+        "io_loop": loop,
+        "error_cb": error_cb,
+    }
+
+    try:
+        yield from nc.connect(**options)
+    except Exception as e:
+        print("ERROR: Could not establish connection to NATS: {}".format(e), file=sys.stderr)
+
+    try:
+        max_messages = int(sys.argv[1])
+    except:
+        max_messages = 1000
+
+    try:
+        max_latency = float(sys.argv[2])
+    except:
+        max_latency = 0.5
+
+    endpoint = '127.0.0.1:8225'
+    httpclient = http.client.HTTPConnection(endpoint)
+    httpclient.request('GET', '/varz')
+    response = httpclient.getresponse()
+    start_varz = json.loads((response.read()).decode())
+
+    start_time = time.time()
+    total_written = 0
+    total_timeouts = 0
+    for i in range(0, max_messages):
+        try:
+            yield from nc.flush(timeout=max_latency)
+            total_written += 1
+        except ErrConnectionClosed as e:
+            break
+        except ErrTimeout as e:
+            total_timeouts += 1
+
+    yield from nc.close()
+    end_time = time.time()
+    duration = end_time - start_time
+    rate = total_written / duration
+
+    httpclient = http.client.HTTPConnection(endpoint)
+    httpclient.request('GET', '/varz')
+    response = httpclient.getresponse()
+    end_varz = json.loads((response.read()).decode())
+
+    delta_varz_in_msgs = end_varz["in_msgs"] - start_varz["in_msgs"]
+    delta_varz_in_bytes = end_varz["in_bytes"] - start_varz["in_bytes"]
+    print("|{0}|{1}|{2}|{3}|{4}|{5}|{6}|{7}|{8}".format(
+        max_messages,
+        max_latency,
+        duration,
+        rate,
+        total_written,
+        total_timeouts,
+        errors,
+        delta_varz_in_msgs,
+        delta_varz_in_bytes,
+        ))
+
+if __name__ == '__main__':
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(run(loop))
+    loop.close()

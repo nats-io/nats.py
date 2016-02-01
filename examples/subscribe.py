2ca06b9c3126ee1e100185d29f25173bf7cd4b8d
Waldemar Quevedo <waldemar.quevedo@gmail.com>
2016-01-31 17:41:57 -0800

    Add subscribe example to repo

--- examples/subscribe.py
+++ examples/subscribe.py
@@ -1,37 +1,54 @@
 import asyncio
+import os
+import signal
 from nats.aio.client import Client as NATS
 
 def run(loop):
-  nc = NATS()
+    nc = NATS()
 
-  options = {
-    "servers": ["nats://127.0.0.1:4222"],
-    "io_loop": loop
-  }
+    @asyncio.coroutine
+    def closed_cb():
+        print("Connection to NATS is closed.")
+        loop.stop()
 
-  yield from nc.connect(**options)
-  print("Connected to NATS at {}...".format(nc.connected_url.netloc))
+    options = {
+        "servers": ["nats://127.0.0.1:4222"],
+        "io_loop": loop,
+        "closed_cb": closed_cb
+    }
 
-  @asyncio.coroutine
-  def subscribe_handler(msg):
-    subject = msg.subject
-    reply = msg.reply
-    data = msg.data.decode()
-    print("Received a message on '{subject} {reply}': {data}".format(
-      subject=subject, reply=reply, data=data))
+    yield from nc.connect(**options)
+    print("Connected to NATS at {}...".format(nc.connected_url.netloc))
 
-  # Basic subscription to receive all published messages
-  # which are being sent to a single topic 'discover'
-  yield from nc.subscribe("discover", cb=subscribe_handler)
+    @asyncio.coroutine
+    def subscribe_handler(msg):
+        subject = msg.subject
+        reply = msg.reply
+        data = msg.data.decode()
+        print("Received a message on '{subject} {reply}': {data}".format(
+          subject=subject, reply=reply, data=data))
 
-  # Subscription on queue named 'workers' so that
-  # one subscriber handles message a request at a time.
-  yield from nc.subscribe("help.*", "workers", subscribe_handler)
+    # Basic subscription to receive all published messages
+    # which are being sent to a single topic 'discover'
+    yield from nc.subscribe("discover", cb=subscribe_handler)
+
+    # Subscription on queue named 'workers' so that
+    # one subscriber handles message a request at a time.
+    yield from nc.subscribe("help.*", "workers", subscribe_handler)
+
+    def signal_handler():
+        if nc.is_closed:
+            return
+        print("Disconnecting...")
+        loop.create_task(nc.close())
+
+    for sig in ('SIGINT', 'SIGTERM'):
+        loop.add_signal_handler(getattr(signal, sig), signal_handler)
 
 if __name__ == '__main__':
-  loop = asyncio.get_event_loop()
-  loop.run_until_complete(run(loop))
-  try:
-      loop.run_forever()
-  finally:
-      loop.close()
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(run(loop))
+    try:
+        loop.run_forever()
+    finally:
+        loop.close()

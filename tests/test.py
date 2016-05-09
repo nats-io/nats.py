d42160360afcd5c1e8a211111d87be919796722e
Waldemar Quevedo <wally@apcera.com>
2016-05-09 00:26:46 -0700

    Add tests showing async/await

--- tests/test.py
+++ tests/test.py
@@ -4,12 +4,19 @@ import unittest
 from tests.parser_test import *
 from tests.client_test import *
 
+if sys.version_info >= (3, 5):
+    from tests.client_async_await_test import *
+
 if __name__ == '__main__':
     test_suite = unittest.TestSuite()
     test_suite.addTest(unittest.makeSuite(ProtocolParserTest))
     test_suite.addTest(unittest.makeSuite(ClientUtilsTest))
     test_suite.addTest(unittest.makeSuite(ClientTest))
     test_suite.addTest(unittest.makeSuite(ClientReconnectTest))
+
+    # Skip tests using async/await syntax unless on Python 3.5
+    if sys.version_info >= (3, 5):
+        test_suite.addTest(unittest.makeSuite(ClientAsyncAwaitTest))
     runner = unittest.TextTestRunner(stream=sys.stdout)
     result = runner.run(test_suite)
     if not result.wasSuccessful():

dd01de5a6a280ae0aca3437f1ec762d0f328e87a
Waldemar Quevedo <wally@apcera.com>
2016-05-10 11:11:25 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,14 +2,21 @@ from setuptools import setup
 from nats.aio.client import __version__
 
 setup(
-  name='asyncio-nats-client',
-  version=__version__,
-  description='NATS client for Python Asyncio',
-  long_description='Asyncio based Python client for NATS, a lightweight, high-performance cloud native messaging system',
-  url='https://github.com/nats-io/asyncio-nats',
-  author='Waldemar Quevedo',
-  author_email='wally@apcera.com',
-  license='MIT License',
-  packages=['nats', 'nats.aio', 'nats.protocol'],
-  zip_safe=True,
+    name='asyncio-nats-client',
+    version=__version__,
+    description='NATS client for Python Asyncio',
+    long_description='Asyncio based Python client for NATS, a lightweight, high-performance cloud native messaging system',
+    classifiers=[
+        'Intended Audience :: Developers',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5'
+        ],
+    url='https://github.com/nats-io/asyncio-nats',
+    author='Waldemar Quevedo',
+    author_email='wally@apcera.com',
+    license='MIT License',
+    packages=['nats', 'nats.aio', 'nats.protocol'],
+    zip_safe=True,
 )

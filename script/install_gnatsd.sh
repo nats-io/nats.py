ea5d1d73e42a5139a29950770143afb4ba953072
Waldemar Quevedo <waldemar.quevedo@gmail.com>
2018-07-05 10:40:28 -0700

    Update server version

--- script/install_gnatsd.sh
+++ script/install_gnatsd.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-export DEFAULT_NATS_SERVER_VERSION=v1.0.4
+export DEFAULT_NATS_SERVER_VERSION=v1.2.0
 export NATS_SERVER_VERSION="${NATS_SERVER_VERSION:=$DEFAULT_NATS_SERVER_VERSION}"
 
 # check to see if gnatsd folder is empty

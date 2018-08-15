8995ddd377bff930f255107f147689d9d32f852a
Waldemar Quevedo <wally@synadia.com>
2018-08-15 10:04:48 -0700

    Add Python 3.7

--- setup.py
+++ setup.py
@@ -10,9 +10,9 @@ setup(
         'Intended Audience :: Developers',
         'Programming Language :: Python',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6'
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7'
         ],
     url='https://github.com/nats-io/asyncio-nats',
     author='Waldemar Quevedo',

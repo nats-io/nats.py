21586f93b1f241fc98d9fb823f7bcc7e69733788
Waldemar Quevedo <wally@synadia.com>
2019-05-13 02:48:36 -0500

    Update example tools to use creds
    Signed-off-by: Waldemar Quevedo <wally@synadia.com>

--- examples/nats-req/__main__.py
+++ examples/nats-req/__main__.py
@@ -1,4 +1,4 @@
-# Copyright 2016-2018 The NATS Authors
+# Copyright 2016-2019 The NATS Authors
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
@@ -20,11 +20,11 @@ from nats.aio.client import Client as NATS
 
 def show_usage():
     usage = """
-nats-sub SUBJECT [-s SERVER] [-q QUEUE]
+nats-req SUBJECT [-d DATA] [-s SERVER]
 
 Example:
 
-nats-sub help -q workers -s nats://127.0.0.1:4222 -s nats://127.0.0.1:4223
+nats-req hello -d world -s nats://127.0.0.1:4222 -s nats://127.0.0.1:4223
 """
     print(usage)
 
@@ -32,39 +32,26 @@ def show_usage_and_die():
     show_usage()
     sys.exit(1)
 
-def run(loop):
+async def run(loop):
     parser = argparse.ArgumentParser()
 
-    # e.g. nats-sub hello -s nats://127.0.0.1:4222
+    # e.g. nats-req hello -d "world" -s nats://127.0.0.1:4222 -s nats://127.0.0.1:4223
     parser.add_argument('subject', default='hello', nargs='?')
+    parser.add_argument('-d', '--data', default="hello world")
     parser.add_argument('-s', '--servers', default=[], action='append')
-    parser.add_argument('-q', '--queue', default="")
     parser.add_argument('--creds', default="")
     args = parser.parse_args()
 
     nc = NATS()
 
-    @asyncio.coroutine
-    def error_cb(e):
+    async def error_cb(e):
         print("Error:", e)
 
-    @asyncio.coroutine
-    def closed_cb():
+    async def closed_cb():
         print("Connection to NATS is closed.")
-        yield from asyncio.sleep(0.1, loop=loop)
-        loop.stop()
 
-    @asyncio.coroutine
-    def reconnected_cb():
+    async def reconnected_cb():
         print("Connected to NATS at {}...".format(nc.connected_url.netloc))
-
-    @asyncio.coroutine
-    def subscribe_handler(msg):
-        subject = msg.subject
-        reply = msg.reply
-        data = msg.data.decode()
-        print("Received a message on '{subject} {reply}': {data}".format(
-          subject=subject, reply=reply, data=data))
 
     options = {
         "io_loop": loop,
@@ -80,27 +67,23 @@ def run(loop):
         if len(args.servers) > 0:
             options['servers'] = args.servers
 
-        yield from nc.connect(**options)
+        await nc.connect(**options)
     except Exception as e:
         print(e)
         show_usage_and_die()
 
     print("Connected to NATS at {}...".format(nc.connected_url.netloc))
-    def signal_handler():
-        if nc.is_closed:
-            return
-        print("Disconnecting...")
-        loop.create_task(nc.close())
-
-    for sig in ('SIGINT', 'SIGTERM'):
-        loop.add_signal_handler(getattr(signal, sig), signal_handler)
-
-    yield from nc.subscribe(args.subject, args.queue, subscribe_handler)
+    msg = await nc.request(args.subject, args.data.encode())
+    subject = msg.subject
+    reply = msg.reply
+    data = msg.data.decode()
+    print("Received a message on '{subject} {reply}': {data}".format(
+        subject=subject, reply=reply, data=data))
+    await nc.close()
 
 if __name__ == '__main__':
     loop = asyncio.get_event_loop()
-    loop.run_until_complete(run(loop))
     try:
-        loop.run_forever()
+        loop.run_until_complete(run(loop))
     finally:
         loop.close()

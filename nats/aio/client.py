14f5db6f7f5395df54ffdc43ff06153b7afaefef
Waldemar Quevedo <wally@synadia.com>
2020-09-11 10:26:03 -0700

    Bump version to v0.11.2
    Signed-off-by: Waldemar Quevedo <wally@synadia.com>

--- nats/aio/client.py
+++ nats/aio/client.py
@@ -28,7 +28,7 @@ from nats.aio.utils import new_inbox
 from nats.aio.nuid import NUID
 from nats.protocol.parser import *
 
-__version__ = '0.11.0'
+__version__ = '0.11.2'
 __lang__ = 'python3'
 _logger = logging.getLogger(__name__)
 PROTOCOL = 1

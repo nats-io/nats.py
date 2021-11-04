d6f0e0b49421775e2b1fdb6bd5f2c292681092af
Waldemar Quevedo <wally@synadia.com>
2021-11-03 17:37:51 -0700

    js: update readme, add example
    Signed-off-by: Waldemar Quevedo <wally@synadia.com>

---
+++ examples/jetstream.py
@@ -0,0 +1,29 @@
+import asyncio
+import nats
+
+async def main():
+    nc = await nats.connect("localhost")
+
+    # Create JetStream context.
+    js = nc.jetstream()
+
+    # Persist messages on 'foo's subject.
+    await js.add_stream(name="sample-stream", subjects=["foo"])
+
+    for i in range(0, 10):
+        ack = await js.publish("foo", f"hello world: {i}".encode())
+        print(ack)
+
+    # Create pull based consumer on 'foo'.
+    psub = await js.pull_subscribe("foo", "psub")
+
+    # Fetch and ack messagess from consumer.
+    for i in range(0, 10):    
+        msgs = await psub.fetch(1)
+        for msg in msgs:
+            print(msg)
+
+    await nc.close()
+
+if __name__ == '__main__':
+    asyncio.run(main())

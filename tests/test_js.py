c98efaaaef4a7096a8417c94bf545d8ddc5fee7f
Waldemar Quevedo <wally@nats.io>
2022-08-15 14:41:06 -0700

    Move test
    Signed-off-by: Waldemar Quevedo <wally@nats.io>

--- tests/test_js.py
+++ tests/test_js.py
@@ -939,6 +939,25 @@ class JSMTest(SingleJetStreamServerTestCase):
 
         await nc.close()
 
+    @async_test
+    async def test_number_of_consumer_replicas(self):
+        nc = await nats.connect()
+
+        js = nc.jetstream()
+        await js.add_stream(name="TESTREPLICAS", subjects=["test.replicas"])
+        for i in range(0, 10):
+            await js.publish("test.replicas", f'{i}'.encode())
+
+        # Create consumer
+        config = nats.js.api.ConsumerConfig(
+            num_replicas=1, durable_name="mycons"
+        )
+        cons = await js.add_consumer(stream="TESTREPLICAS", config=config)
+        if cons.config.num_replicas:
+            assert cons.config.num_replicas == 1
+
+        await nc.close()
+
 
 class SubscribeTest(SingleJetStreamServerTestCase):
 
@@ -1689,25 +1708,3 @@ class KVTest(SingleJetStreamServerTestCase):
 
         with pytest.raises(BadBucketError):
             await js.key_value(bucket="TEST3")
-
-
-class ConsumerReplicasTest(SingleJetStreamServerTestCase):
-
-    @async_test
-    async def test_number_of_consumer_replicas(self):
-        nc = await nats.connect()
-
-        js = nc.jetstream()
-        await js.add_stream(name="TESTREPLICAS", subjects=["test.replicas"])
-        for i in range(0, 10):
-            await js.publish("test.replicas", f'{i}'.encode())
-
-        # Create consumer
-        config = nats.js.api.ConsumerConfig(
-            num_replicas=1, durable_name="mycons"
-        )
-        cons = await js.add_consumer(stream="TESTREPLICAS", config=config)
-
-        assert cons.config.num_replicas == 1
-
-        await nc.close()

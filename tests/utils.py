c72de758b7c053292b8cd005ba3fd5a5fb5f8e7c
Brunno Vanelli <brunnovanelli@gmail.com>
2022-09-20 19:12:52 +0200

    Add tests as a copy of current implementation for test client

--- tests/utils.py
+++ tests/utils.py
@@ -444,6 +444,24 @@ class SingleJetStreamServerTestCase(unittest.TestCase):
         self.loop.close()
 
 
+class SingleWebsocketServerTestCase(unittest.TestCase):
+
+    def setUp(self):
+        self.server_pool = []
+        self.loop = asyncio.new_event_loop()
+
+        server = NATSD(port=4222, config_file=get_config_file("websocket/ws.conf"))
+        self.server_pool.append(server)
+        for natsd in self.server_pool:
+            start_natsd(natsd)
+
+    def tearDown(self):
+        for natsd in self.server_pool:
+            natsd.stop()
+            shutil.rmtree(natsd.store_dir)
+        self.loop.close()
+
+
 def start_natsd(natsd: NATSD):
     natsd.start()
 

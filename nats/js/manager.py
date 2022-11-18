2d1eb872d31667c8cdcf00faa18123ed5c5a9401
gram <git@orsinium.dev>
2022-11-18 14:58:13 +0100

    format code with yapf

--- nats/js/manager.py
+++ nats/js/manager.py
@@ -76,7 +76,9 @@ class JetStreamManager:
         return api.StreamInfo.from_response(resp)
 
     async def add_stream(
-        self, config: Optional[api.StreamConfig] = None, **params
+        self,
+        config: Optional[api.StreamConfig] = None,
+        **params
     ) -> api.StreamInfo:
         """
         add_stream creates a stream.
@@ -96,7 +98,9 @@ class JetStreamManager:
         return api.StreamInfo.from_response(resp)
 
     async def update_stream(
-        self, config: Optional[api.StreamConfig] = None, **params
+        self,
+        config: Optional[api.StreamConfig] = None,
+        **params
     ) -> api.StreamInfo:
         """
         update_stream updates a stream.

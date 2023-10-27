47b5838f9408b646a912e0280a7bbc6cc6b8f7b8
Waldemar Quevedo <wally@nats.io>
2023-10-27 16:18:21 -0700

    Bump version to v2.6.0
    Signed-off-by: Waldemar Quevedo <wally@nats.io>

--- nats/aio/client.py
+++ nats/aio/client.py
@@ -65,7 +65,7 @@ from .subscription import (
 )
 from .transport import TcpTransport, Transport, WebSocketTransport
 
-__version__ = '2.5.0'
+__version__ = '2.6.0'
 __lang__ = 'python3'
 _logger = logging.getLogger(__name__)
 PROTOCOL = 1

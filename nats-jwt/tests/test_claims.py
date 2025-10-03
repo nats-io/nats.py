26faa1dcd565c5f72eaa51828b7aec3a6ea06d6f
Casper Beyer <casper@synadia.com>
2025-10-03 20:35:24 +0200

    Transition to flat tests

--- nats-jwt/tests/test_claims.py
+++ nats-jwt/tests/test_claims.py
@@ -1,5 +1,3 @@
-import asyncio
-
 import pytest
 
 nkeys_installed = None
@@ -21,147 +19,135 @@ from nats.jwt.claims.models import Claims
 from nats.jwt.types import Permission, Permissions, Types
 from nats.jwt.users.limits import UserPermissionsLimits
 from nats.jwt.users.models import User
-from tests.utils import (
-    NkeysServerTestCase,
-    SingleServerTestCase,
-    TrustedServerTestCase,
-    async_test,
-    get_config_file,
-)
 
 
-class ClaimsTest(SingleServerTestCase):
+def test_account_claims():
 
-    def test_account_claims(self):
+    if not nkeys_installed:
+        pytest.skip("nkeys not installed")
 
-        if not nkeys_installed:
-            pytest.skip("nkeys not installed")
+    from nats.jwt.nkeys import from_seed
 
-        from nats.jwt.nkeys import from_seed
+    operatorKP = from_seed(
+        b'SOALU7LPGJK2BDF7IHD7UZT6ZM23UMKYLGJLNN35QJSUI5BNR4DJRFH4R4'
+    )
+    accountKP = from_seed(
+        b'SAALXUEDN2QR5KZDDSH5S4RIWAZDM7CVDG5HNJI2HS5LBVYFTLAQCOXZAU'
+    )
+    userKP = from_seed(
+        b'SUALJTG5JNRQCQKFE652DV4XID522ALOHJNQVHKKDJNVGWHCLHOEXEROEM'
+    )
 
-        operatorKP = from_seed(
-            b'SOALU7LPGJK2BDF7IHD7UZT6ZM23UMKYLGJLNN35QJSUI5BNR4DJRFH4R4'
-        )
-        accountKP = from_seed(
-            b'SAALXUEDN2QR5KZDDSH5S4RIWAZDM7CVDG5HNJI2HS5LBVYFTLAQCOXZAU'
-        )
-        userKP = from_seed(
-            b'SUALJTG5JNRQCQKFE652DV4XID522ALOHJNQVHKKDJNVGWHCLHOEXEROEM'
-        )
-
-        account_claims = Claims(
-            name="my-account",
-            jti="PBFES33GGIFZM6UGC7NY5ARHRBFVFU4UD7FS2WNLZH3KPGWFVEFQ",
-            iat=1678973945,
-            iss=operatorKP.public_key.decode(),
-            sub=accountKP.public_key.decode(),
-            nats=Account(
-                limits=OperatorLimits(
-                    nats_limits=NatsLimits(data=-1, payload=-1, subs=-1),
-                    account_limits=AccountLimits(
-                        exports=-1,
-                        imports=-1,
-                        wildcards=True,
-                        conn=-1,
-                        leaf=-1
-                    ),
-                    jetstream_limits=JetStreamLimits(
-                        disk_storage=-1, mem_storage=-1
-                    )
+    account_claims = Claims(
+        name="my-account",
+        jti="PBFES33GGIFZM6UGC7NY5ARHRBFVFU4UD7FS2WNLZH3KPGWFVEFQ",
+        iat=1678973945,
+        iss=operatorKP.public_key.decode(),
+        sub=accountKP.public_key.decode(),
+        nats=Account(
+            limits=OperatorLimits(
+                nats_limits=NatsLimits(data=-1, payload=-1, subs=-1),
+                account_limits=AccountLimits(
+                    exports=-1,
+                    imports=-1,
+                    wildcards=True,
+                    conn=-1,
+                    leaf=-1
                 ),
-                default_permissions=Permissions(),
-                generic_fields=GenericFields(version=2, type=Types.Account)
-            )
+                jetstream_limits=JetStreamLimits(
+                    disk_storage=-1, mem_storage=-1
+                )
+            ),
+            default_permissions=Permissions(),
+            generic_fields=GenericFields(version=2, type=Types.Account)
         )
+    )
 
-        self.assertEqual(
-            account_claims.to_dict(), {
-                "iat": 1678973945,
-                "name": "my-account",
-                "iss":
-                    "OCCKR76QCKV4R224WP6ZISXWLXLJZWDF22TRZFQM2I6KUPEDQ3OVCJ6N",
-                "jti": "PBFES33GGIFZM6UGC7NY5ARHRBFVFU4UD7FS2WNLZH3KPGWFVEFQ",
-                "sub":
-                    "AB5D6N64ZGUTCGETBW3HSORLTJH5UCCB5CKPZFWCF6UV3KI5BCTRPFDC",
-                "nats": {
-                    "default_permissions": {
-                        "pub": {},
-                        "sub": {}
-                    },
-                    "limits": {
-                        "conn": -1,
-                        "data": -1,
-                        "disk_storage": -1,
-                        "wildcards": True,
-                        "exports": -1,
-                        "imports": -1,
-                        "leaf": -1,
-                        "mem_storage": -1,
-                        "payload": -1,
-                        "subs": -1,
-                    },
-                    "type": Types.Account,
-                    "version": 2
-                },
-            }
+    assert account_claims.to_dict() == {
+        "iat": 1678973945,
+        "name": "my-account",
+        "iss":
+            "OCCKR76QCKV4R224WP6ZISXWLXLJZWDF22TRZFQM2I6KUPEDQ3OVCJ6N",
+        "jti": "PBFES33GGIFZM6UGC7NY5ARHRBFVFU4UD7FS2WNLZH3KPGWFVEFQ",
+        "sub":
+            "AB5D6N64ZGUTCGETBW3HSORLTJH5UCCB5CKPZFWCF6UV3KI5BCTRPFDC",
+        "nats": {
+            "default_permissions": {
+                "pub": {},
+                "sub": {}
+            },
+            "limits": {
+                "conn": -1,
+                "data": -1,
+                "disk_storage": -1,
+                "wildcards": True,
+                "exports": -1,
+                "imports": -1,
+                "leaf": -1,
+                "mem_storage": -1,
+                "payload": -1,
+                "subs": -1,
+            },
+            "type": Types.Account,
+            "version": 2
+        },
+    }
+
+
+def test_user_claims():
+
+    if not nkeys_installed:
+        pytest.skip("nkeys not installed")
+
+    from nats.jwt.nkeys import from_seed
+
+    operatorKP = from_seed(
+        b'SOALU7LPGJK2BDF7IHD7UZT6ZM23UMKYLGJLNN35QJSUI5BNR4DJRFH4R4'
+    )
+    accountKP = from_seed(
+        b'SAALXUEDN2QR5KZDDSH5S4RIWAZDM7CVDG5HNJI2HS5LBVYFTLAQCOXZAU'
+    )
+    userKP = from_seed(
+        b'SUALJTG5JNRQCQKFE652DV4XID522ALOHJNQVHKKDJNVGWHCLHOEXEROEM'
+    )
+
+    user_claims = Claims(
+        name="my-user",
+        jti="OJZUDMVGC5WOWNEVFJOEBQG7CGNVRSEZUPM7KTRLE26TX7OPH3TQ",
+        iat=1678973945,
+        iss=accountKP.public_key.decode(),
+        sub=userKP.public_key.decode(),
+        nats=User(
+            permissions_limits=UserPermissionsLimits(
+                limits=NatsLimits(data=1073741824, payload=-1, subs=-1),
+                permissions=Permissions(
+                    pub=Permission(allow=["foo.>", "bar.>"], deny=None),
+                    sub=Permission(allow=["_INBOX.>"], deny=None),
+                )
+            ),
+            generic_fields=GenericFields(version=2, type=Types.User)
         )
+    )
 
-    def test_user_claims(self):
-
-        if not nkeys_installed:
-            pytest.skip("nkeys not installed")
-
-        from nats.jwt.nkeys import from_seed
-
-        operatorKP = from_seed(
-            b'SOALU7LPGJK2BDF7IHD7UZT6ZM23UMKYLGJLNN35QJSUI5BNR4DJRFH4R4'
-        )
-        accountKP = from_seed(
-            b'SAALXUEDN2QR5KZDDSH5S4RIWAZDM7CVDG5HNJI2HS5LBVYFTLAQCOXZAU'
-        )
-        userKP = from_seed(
-            b'SUALJTG5JNRQCQKFE652DV4XID522ALOHJNQVHKKDJNVGWHCLHOEXEROEM'
-        )
-
-        user_claims = Claims(
-            name="my-user",
-            jti="OJZUDMVGC5WOWNEVFJOEBQG7CGNVRSEZUPM7KTRLE26TX7OPH3TQ",
-            iat=1678973945,
-            iss=accountKP.public_key.decode(),
-            sub=userKP.public_key.decode(),
-            nats=User(
-                permissions_limits=UserPermissionsLimits(
-                    limits=NatsLimits(data=1073741824, payload=-1, subs=-1),
-                    permissions=Permissions(
-                        pub=Permission(allow=["foo.>", "bar.>"], deny=None),
-                        sub=Permission(allow=["_INBOX.>"], deny=None),
-                    )
-                ),
-                generic_fields=GenericFields(version=2, type=Types.User)
-            )
-        )
-
-        self.assertEqual(
-            user_claims.to_dict(), {
-                "iat": 1678973945,
-                "iss":
-                    "AB5D6N64ZGUTCGETBW3HSORLTJH5UCCB5CKPZFWCF6UV3KI5BCTRPFDC",
-                "sub":
-                    "UATWOEX5T5LGWJ54SC7H762G5PKSSFPVTJX67IUFENTVPYHA4DPDJUZU",
-                "jti": "OJZUDMVGC5WOWNEVFJOEBQG7CGNVRSEZUPM7KTRLE26TX7OPH3TQ",
-                "name": "my-user",
-                "nats": {
-                    "data": 1073741824,
-                    "payload": -1,
-                    "pub": {
-                        "allow": ["foo.>", "bar.>"]
-                    },
-                    "sub": {
-                        "allow": ["_INBOX.>"]
-                    },
-                    "subs": -1,
-                    "type": Types.User,
-                    "version": 2
-                },
-            }
-        )
+    assert user_claims.to_dict() == {
+        "iat": 1678973945,
+        "iss":
+            "AB5D6N64ZGUTCGETBW3HSORLTJH5UCCB5CKPZFWCF6UV3KI5BCTRPFDC",
+        "sub":
+            "UATWOEX5T5LGWJ54SC7H762G5PKSSFPVTJX67IUFENTVPYHA4DPDJUZU",
+        "jti": "OJZUDMVGC5WOWNEVFJOEBQG7CGNVRSEZUPM7KTRLE26TX7OPH3TQ",
+        "name": "my-user",
+        "nats": {
+            "data": 1073741824,
+            "payload": -1,
+            "pub": {
+                "allow": ["foo.>", "bar.>"]
+            },
+            "sub": {
+                "allow": ["_INBOX.>"]
+            },
+            "subs": -1,
+            "type": Types.User,
+            "version": 2
+        },
+    }
--- nats-jwt/tests/test_jwt.py
+++ nats-jwt/tests/test_jwt.py
@@ -1,4 +1,5 @@
-import asyncio
+import base64
+import json
 
 import pytest
 
@@ -9,12 +10,8 @@ try:
 except ModuleNotFoundError:
     nkeys_installed = False
 
-import base64
-import json
-
 from nats.jwt.jwt import Algorithms
 from nats.jwt.utils import bytes_serializer
-from tests.utils import SingleServerTestCase, async_test
 
 
 def encode_account_claim(account_claims, operator_key_pair, options):
@@ -121,107 +118,100 @@ def encode_user_claim(user_claims, account_key_pair, options):
     return jwt_token
 
 
-class JWTTest(SingleServerTestCase):
+def test_account_claim_jwt():
+    # NOTE: Test replicated from https://natsbyexample.com/examples/auth/nkeys-jwts/go
 
-    def test_account_claim_jwt(self):
-        # NOTE: Test replicated from https://natsbyexample.com/examples/auth/nkeys-jwts/go
+    if not nkeys_installed:
+        pytest.skip("nkeys not installed")
 
-        if not nkeys_installed:
-            pytest.skip("nkeys not installed")
+    from nats.jwt.nkeys import from_seed
 
-        from nats.jwt.nkeys import from_seed
+    operatorKP = from_seed(
+        b'SOALU7LPGJK2BDF7IHD7UZT6ZM23UMKYLGJLNN35QJSUI5BNR4DJRFH4R4'
+    )
+    accountKP = from_seed(
+        b'SAALXUEDN2QR5KZDDSH5S4RIWAZDM7CVDG5HNJI2HS5LBVYFTLAQCOXZAU'
+    )
+    userKP = from_seed(
+        b'SUALJTG5JNRQCQKFE652DV4XID522ALOHJNQVHKKDJNVGWHCLHOEXEROEM'
+    )
 
-        operatorKP = from_seed(
-            b'SOALU7LPGJK2BDF7IHD7UZT6ZM23UMKYLGJLNN35QJSUI5BNR4DJRFH4R4'
-        )
-        accountKP = from_seed(
-            b'SAALXUEDN2QR5KZDDSH5S4RIWAZDM7CVDG5HNJI2HS5LBVYFTLAQCOXZAU'
-        )
-        userKP = from_seed(
-            b'SUALJTG5JNRQCQKFE652DV4XID522ALOHJNQVHKKDJNVGWHCLHOEXEROEM'
-        )
-
-        account_claim = {
-            "name": "my-account",
-            "nats": {
-                "default_permissions": {
-                    "pub": {},
-                    "sub": {}
-                },
-                "limits": {
-                    "conn": -1,
-                    "data": -1,
-                    "disk_storage": -1,
-                    "exports": -1,
-                    "imports": -1,
-                    "leaf": -1,
-                    "mem_storage": -1,
-                    "payload": -1,
-                    "subs": -1,
-                    "wildcards": True
-                }
+    account_claim = {
+        "name": "my-account",
+        "nats": {
+            "default_permissions": {
+                "pub": {},
+                "sub": {}
             },
-            "sub": accountKP.public_key.decode()
-        }
-        opts = {
-            "jti": "PBFES33GGIFZM6UGC7NY5ARHRBFVFU4UD7FS2WNLZH3KPGWFVEFQ",
-            "iat": 1678973945,
-            "iss": operatorKP.public_key.decode(),
-            "type": "account",
-            "version": 2
-        }
-        account_claim_jwt_token = encode_account_claim(
-            account_claim, operatorKP, options=opts
-        )
-
-        self.assertEqual(
-            account_claim_jwt_token,
-            "eyJ0eXAiOiJKV1QiLCJhbGciOiJlZDI1NTE5LW5rZXkifQ.eyJqdGkiOiJQQkZFUzMzR0dJRlpNNlVHQzdOWTVBUkhSQkZWRlU0VUQ3RlMyV05MWkgzS1BHV0ZWRUZRIiwiaWF0IjoxNjc4OTczOTQ1LCJpc3MiOiJPQ0NLUjc2UUNLVjRSMjI0V1A2WklTWFdMWExKWldERjIyVFJaRlFNMkk2S1VQRURRM09WQ0o2TiIsIm5hbWUiOiJteS1hY2NvdW50Iiwic3ViIjoiQUI1RDZONjRaR1VUQ0dFVEJXM0hTT1JMVEpINVVDQ0I1Q0tQWkZXQ0Y2VVYzS0k1QkNUUlBGREMiLCJuYXRzIjp7ImxpbWl0cyI6eyJzdWJzIjotMSwiZGF0YSI6LTEsInBheWxvYWQiOi0xLCJpbXBvcnRzIjotMSwiZXhwb3J0cyI6LTEsIndpbGRjYXJkcyI6dHJ1ZSwiY29ubiI6LTEsImxlYWYiOi0xLCJtZW1fc3RvcmFnZSI6LTEsImRpc2tfc3RvcmFnZSI6LTF9LCJkZWZhdWx0X3Blcm1pc3Npb25zIjp7InB1YiI6e30sInN1YiI6e319LCJ0eXBlIjoiYWNjb3VudCIsInZlcnNpb24iOjJ9fQ.4-kUapoPK_9A9L_CfJRBEe1XukgVBGaSU3J5tBFbajF3G5660BORa2CRUnN6x0dv-jUgui5EIQeANDB5kh_wDw"
-        )
-
-    def test_user_claim_jwt(self):
-        if not nkeys_installed:
-            pytest.skip("nkeys not installed")
-
-        from nats.jwt.nkeys import from_seed
-
-        operatorKP = from_seed(
-            b'SOALU7LPGJK2BDF7IHD7UZT6ZM23UMKYLGJLNN35QJSUI5BNR4DJRFH4R4'
-        )
-        accountKP = from_seed(
-            b'SAALXUEDN2QR5KZDDSH5S4RIWAZDM7CVDG5HNJI2HS5LBVYFTLAQCOXZAU'
-        )
-        userKP = from_seed(
-            b'SUALJTG5JNRQCQKFE652DV4XID522ALOHJNQVHKKDJNVGWHCLHOEXEROEM'
-        )
-
-        user_claim = {
-            "name": "my-user",
-            "sub": userKP.public_key.decode(),
-            "nats": {
-                "pub": {
-                    "allow": ["foo.>", "bar.>"]
-                },
-                "sub": {
-                    "allow": ["_INBOX.>"]
-                },
-                "subs": -1,
-                "data": 1073741824,
+            "limits": {
+                "conn": -1,
+                "data": -1,
+                "disk_storage": -1,
+                "exports": -1,
+                "imports": -1,
+                "leaf": -1,
+                "mem_storage": -1,
                 "payload": -1,
+                "subs": -1,
+                "wildcards": True
             }
-        }
-        opts = {
-            "jti": "OJZUDMVGC5WOWNEVFJOEBQG7CGNVRSEZUPM7KTRLE26TX7OPH3TQ",
-            "iat": 1678973945,
-            "iss": accountKP.public_key.decode(),
-            "type": "user",
-            "version": 2
-        }
-        user_claim_jwt_token = encode_user_claim(
-            user_claim, accountKP, options=opts
-        )
+        },
+        "sub": accountKP.public_key.decode()
+    }
+    opts = {
+        "jti": "PBFES33GGIFZM6UGC7NY5ARHRBFVFU4UD7FS2WNLZH3KPGWFVEFQ",
+        "iat": 1678973945,
+        "iss": operatorKP.public_key.decode(),
+        "type": "account",
+        "version": 2
+    }
+    account_claim_jwt_token = encode_account_claim(
+        account_claim, operatorKP, options=opts
+    )
 
-        self.assertEqual(
-            user_claim_jwt_token,
-            "eyJ0eXAiOiJKV1QiLCJhbGciOiJlZDI1NTE5LW5rZXkifQ.eyJqdGkiOiJPSlpVRE1WR0M1V09XTkVWRkpPRUJRRzdDR05WUlNFWlVQTTdLVFJMRTI2VFg3T1BIM1RRIiwiaWF0IjoxNjc4OTczOTQ1LCJpc3MiOiJBQjVENk42NFpHVVRDR0VUQlczSFNPUkxUSkg1VUNDQjVDS1BaRldDRjZVVjNLSTVCQ1RSUEZEQyIsIm5hbWUiOiJteS11c2VyIiwic3ViIjoiVUFUV09FWDVUNUxHV0o1NFNDN0g3NjJHNVBLU1NGUFZUSlg2N0lVRkVOVFZQWUhBNERQREpVWlUiLCJuYXRzIjp7InB1YiI6eyJhbGxvdyI6WyJmb28uXHUwMDNlIiwiYmFyLlx1MDAzZSJdfSwic3ViIjp7ImFsbG93IjpbIl9JTkJPWC5cdTAwM2UiXX0sInN1YnMiOi0xLCJkYXRhIjoxMDczNzQxODI0LCJwYXlsb2FkIjotMSwidHlwZSI6InVzZXIiLCJ2ZXJzaW9uIjoyfX0.PYT1aJJXiJd9Jb5b1m03jBs64GJzjKRtLOH4hoKJ8v8MKk13nhzGFtKcIKn2vg00uYBYlOkWPgzJ6hYuKr0ECA"
-        )
+    assert account_claim_jwt_token == "eyJ0eXAiOiJKV1QiLCJhbGciOiJlZDI1NTE5LW5rZXkifQ.eyJqdGkiOiJQQkZFUzMzR0dJRlpNNlVHQzdOWTVBUkhSQkZWRlU0VUQ3RlMyV05MWkgzS1BHV0ZWRUZRIiwiaWF0IjoxNjc4OTczOTQ1LCJpc3MiOiJPQ0NLUjc2UUNLVjRSMjI0V1A2WklTWFdMWExKWldERjIyVFJaRlFNMkk2S1VQRURRM09WQ0o2TiIsIm5hbWUiOiJteS1hY2NvdW50Iiwic3ViIjoiQUI1RDZONjRaR1VUQ0dFVEJXM0hTT1JMVEpINVVDQ0I1Q0tQWkZXQ0Y2VVYzS0k1QkNUUlBGREMiLCJuYXRzIjp7ImxpbWl0cyI6eyJzdWJzIjotMSwiZGF0YSI6LTEsInBheWxvYWQiOi0xLCJpbXBvcnRzIjotMSwiZXhwb3J0cyI6LTEsIndpbGRjYXJkcyI6dHJ1ZSwiY29ubiI6LTEsImxlYWYiOi0xLCJtZW1fc3RvcmFnZSI6LTEsImRpc2tfc3RvcmFnZSI6LTF9LCJkZWZhdWx0X3Blcm1pc3Npb25zIjp7InB1YiI6e30sInN1YiI6e319LCJ0eXBlIjoiYWNjb3VudCIsInZlcnNpb24iOjJ9fQ.4-kUapoPK_9A9L_CfJRBEe1XukgVBGaSU3J5tBFbajF3G5660BORa2CRUnN6x0dv-jUgui5EIQeANDB5kh_wDw"
+
+
+def test_user_claim_jwt():
+    if not nkeys_installed:
+        pytest.skip("nkeys not installed")
+
+    from nats.jwt.nkeys import from_seed
+
+    operatorKP = from_seed(
+        b'SOALU7LPGJK2BDF7IHD7UZT6ZM23UMKYLGJLNN35QJSUI5BNR4DJRFH4R4'
+    )
+    accountKP = from_seed(
+        b'SAALXUEDN2QR5KZDDSH5S4RIWAZDM7CVDG5HNJI2HS5LBVYFTLAQCOXZAU'
+    )
+    userKP = from_seed(
+        b'SUALJTG5JNRQCQKFE652DV4XID522ALOHJNQVHKKDJNVGWHCLHOEXEROEM'
+    )
+
+    user_claim = {
+        "name": "my-user",
+        "sub": userKP.public_key.decode(),
+        "nats": {
+            "pub": {
+                "allow": ["foo.>", "bar.>"]
+            },
+            "sub": {
+                "allow": ["_INBOX.>"]
+            },
+            "subs": -1,
+            "data": 1073741824,
+            "payload": -1,
+        }
+    }
+    opts = {
+        "jti": "OJZUDMVGC5WOWNEVFJOEBQG7CGNVRSEZUPM7KTRLE26TX7OPH3TQ",
+        "iat": 1678973945,
+        "iss": accountKP.public_key.decode(),
+        "type": "user",
+        "version": 2
+    }
+    user_claim_jwt_token = encode_user_claim(
+        user_claim, accountKP, options=opts
+    )
+
+    assert user_claim_jwt_token == "eyJ0eXAiOiJKV1QiLCJhbGciOiJlZDI1NTE5LW5rZXkifQ.eyJqdGkiOiJPSlpVRE1WR0M1V09XTkVWRkpPRUJRRzdDR05WUlNFWlVQTTdLVFJMRTI2VFg3T1BIM1RRIiwiaWF0IjoxNjc4OTczOTQ1LCJpc3MiOiJBQjVENk42NFpHVVRDR0VUQlczSFNPUkxUSkg1VUNDQjVDS1BaRldDRjZVVjNLSTVCQ1RSUEZEQyIsIm5hbWUiOiJteS11c2VyIiwic3ViIjoiVUFUV09FWDVUNUxHV0o1NFNDN0g3NjJHNVBLU1NGUFZUSlg2N0lVRkVOVFZQWUhBNERQREpVWlUiLCJuYXRzIjp7InB1YiI6eyJhbGxvdyI6WyJmb28uXHUwMDNlIiwiYmFyLlx1MDAzZSJdfSwic3ViIjp7ImFsbG93IjpbIl9JTkJPWC5cdTAwM2UiXX0sInN1YnMiOi0xLCJkYXRhIjoxMDczNzQxODI0LCJwYXlsb2FkIjotMSwidHlwZSI6InVzZXIiLCJ2ZXJzaW9uIjoyfX0.PYT1aJJXiJd9Jb5b1m03jBs64GJzjKRtLOH4hoKJ8v8MKk13nhzGFtKcIKn2vg00uYBYlOkWPgzJ6hYuKr0ECA"

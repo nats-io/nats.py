c73b079852806a305071a6965b1779d1b26d4c54
Casper Beyer <casper@synadia.com>
2026-04-03 17:54:11 +0200

    Address review comments

--- nats/src/nats/aio/subscription.py
+++ nats/src/nats/aio/subscription.py
@@ -15,6 +15,7 @@
 from __future__ import annotations
 
 import asyncio
+import logging
 from typing import (
     TYPE_CHECKING,
     AsyncIterator,
@@ -32,6 +33,8 @@ from nats.aio.msg import Msg
 
 if TYPE_CHECKING:
     from nats.js import JetStreamContext
+
+_logger = logging.getLogger(__name__)
 
 DEFAULT_SUB_PENDING_MSGS_LIMIT = 512 * 1024
 DEFAULT_SUB_PENDING_BYTES_LIMIT = 128 * 1024 * 1024
@@ -315,7 +318,10 @@ class Subscription:
                     # All errors from calling a handler
                     # are async errors.
                     if error_cb:
-                        await error_cb(e)
+                        try:
+                            await error_cb(e)
+                        except Exception:
+                            _logger.error("nats: error in error callback", exc_info=True)
                 finally:
                     # indicate the message finished processing so drain can continue.
                     self._pending_queue.task_done()
